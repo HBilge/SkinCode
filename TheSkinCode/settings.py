04aed258668331cc7eadcdf1d6f3665b7746d85b
Bilge Hicyilmam <bilge.hicyilmam@boun.edu.tr>
2021-01-07 10:49:38 +0300

    Fix deployment

--- TheSkinCode/settings.py
+++ TheSkinCode/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,5 +137,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+django_heroku.settings(locals())
 
 BASE_URL = "https://cloud.mongodb.com/"
